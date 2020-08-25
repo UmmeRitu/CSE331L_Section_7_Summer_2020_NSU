4d0f4dbee965144ac61a11a2b3f3c8524b64af53
UmmeRitu <umme.ritu@northsouth.edu>
2020-08-25 10:09:28 +0600

    lab task 2

---
+++ Lab_Task/Lab_5/Lab_Task_2.asm
@@ -0,0 +1,69 @@
+
+; You may customize this and other start-up templates; 
+; The location of this template is c:\emu8086\inc\0_com_template.txt
+
+org 100h
+
+; add your code here
+.MODEL SMALL
+.STACK 100H
+.DATA
+ PROMPT DB 'Enter the digit : $'
+ MSG DB 'The entered digit is : $'
+.CODE
+ MAIN PROC
+ MOV AX, @DATA 
+ MOV DS, AX
+
+ LEA DX, PROMPT 
+ MOV AH, 9
+ INT 21H
+
+ MOV AH, 1  
+ INT 21H
+
+ MOV BL, AL
+  
+ MOV AH, 2 
+ MOV DL, 0DH
+ INT 21H
+
+ MOV DL, 0AH
+ INT 21H
+
+ LEA DX, MSG 
+ MOV AH, 9
+ INT 21H
+
+ CMP BL, 30H
+ 
+ JL @NEGATIVE 
+ JZ @ZERO 
+ JG @POSITIVE 
+
+ @NEGATIVE: 
+ MOV DL, 'N'
+ JMP @DISPLAY
+ 
+ @ZERO: 
+ MOV DL, 'Z'
+ JMP @DISPLAY 
+
+ @POSITIVE: 
+ MOV DL, 'P'
+ JMP @DISPLAY
+
+ @DISPLAY: 
+ MOV AH, 2 
+ INT 21H
+
+ MOV AH, 4CH 
+ INT 21H
+ MAIN ENDP
+END MAIN
+
+ret
+
+
+
+
